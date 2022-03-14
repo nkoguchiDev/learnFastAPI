fec38af66644b9400188bdfd61ffbf15718cbcce
nkoguchi <nkoguchi00dev@gmail.com>
2022-03-15 00:12:27 +0900

    WIP

---
+++ learnFastAPI/app/tests/test_main.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    """GET /message へのREST APIコールで、
+        1. レスポンスコードが200であること
+        2. {"msg": "Hello FASTAPI"}が返却されること
+    """
+    response = client.get("/message")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
