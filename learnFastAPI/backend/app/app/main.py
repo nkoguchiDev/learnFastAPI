0254016ac9d29ab2825307ccd2dbe49f47f1f4ab
nkoguchi <nkoguchi00dev@gmail.com>
2022-03-16 23:34:59 +0900

    :tada: Initial commit frontend src

--- learnFastAPI/backend/app/app/main.py
+++ learnFastAPI/backend/app/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/message")
